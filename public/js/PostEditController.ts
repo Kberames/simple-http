0430d118f1a16213ab1035a73ca51ba54fa7845c
Kimberly Ames <Kimberly Ames>
2016-11-11 14:52:11 -0700

    posteditcontroller changes

--- public/js/PostEditController.ts
+++ public/js/PostEditController.ts
@@ -5,12 +5,26 @@ namespace App {
         private httpService;
         private stateService;
 
+        public post;
+
         constructor (
             $http: angular.IHttpService,
             $state: angular.ui.IState
         ) {
             this.httpService = $http;
             this.stateService = $state;
+
+            console.log ('Passed parameters: ', this.stateService.params);
+            this.httpService ({
+              url: '/posts/' + this.stateService.params.id,
+              method: 'GET'
+            })
+            .success ((response) => {
+              console.log (response);
+              this.post = response;
+            })
+            .error (() => {
+            })
         }
     }
-}
+  }
