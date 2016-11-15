c4444702be8f1eb09ac7318d7b987498b6bf37ec
Kimberly Ames <Kimberly Ames>
2016-11-15 10:23:24 -0700

    added post list display to home page

--- public/js/HomeController.ts
+++ public/js/HomeController.ts
@@ -1,5 +1,32 @@
 namespace App {
     export class HomeController {
-        
+        static $inject = ['$http'];
+
+        private httpService;
+
+        public postList;
+
+                constructor (
+                    $http: angular.IHttpService,
+                ) {
+                    this.httpService = $http;
+
+                    this.getPostList();
+
+                }
+
+        public getPostList () {
+            this.httpService ({
+                url: '/posts',
+                method: 'GET'
+            })
+            .success ((response)=> {
+                this.postList = response;
+
+            })
+            .error ((response)=> {
+
+            })
+        }
     }
 }
